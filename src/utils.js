5094d5732a0a9c7efbe29d6917019bab9b3f58d5
Amatya Rojak <ramatya@lx9-9815-004.lib.helsinki.fi>
2019-06-20 15:03:33 +0300

    added crowd

---
+++ src/utils.js
@@ -0,0 +1,48 @@
+/**
+ *
+ * @licstart  The following is the entire license notice for the JavaScript code in this file.
+ *
+ * UI microservice of Identifier Services
+ *
+ * Copyright (C) 2019 University Of Helsinki (The National Library Of Finland)
+ *
+ * This file is part of identifier-services-ui
+ *
+ * identifier-services-ui program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * identifier-services-ui is distributed in the hope that it will be useful,
+	console.log(values);
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+	console.log(values);
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this file.
+ *
+ */
+
+export function readEnvironmentVariable(name, {defaultValue = undefined, hideDefault = false, format = v => v} = {}) {
+	if (process.env[name] === undefined) {
+		if (defaultValue === undefined) {
+			throw new Error(`Mandatory environment variable missing: ${name}`);
+		}
+
+		const defaultValuePrintable = typeof defaultValue === 'object' ? JSON.stringify(defaultValue) : defaultValue;
+
+		console.error(`No environment variable set for ${name}, using default value: ${hideDefault ? '[hidden]' : defaultValuePrintable}`);
+		return defaultValue;
+	}
+
+	return format(process.env[name]);
+}
+
+export function clone(o) {
+	return JSON.parse(JSON.stringify(o));
+}
